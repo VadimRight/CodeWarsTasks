cd3c515623404a5d74b27ea0a7e086265d447974
vadimright <vadim.komashko@bk.ru>
2023-09-27 17:24:35 +0300

    Cut the Ropes (fast)

--- CodeWars/Cut_the_Ropes.py
+++ CodeWars/Cut_the_Ropes.py
@@ -2,7 +2,6 @@ def cut_the_ropes(arr):
     count_list = []
     while arr:
         count_list.append(len(arr))
-        arr = [a - min(arr) for a in arr]
-        arr = [a for a in arr if a > 0]
+        min_length = min(arr)
+        arr = [a - min_length for a in arr if a > min_length]
     return count_list
-    pass
