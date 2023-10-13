dbd5ac73b3bf6cdae9db0011318e7b6b92f5474b
vadimright <vadim.komashko@bk.ru>
2023-10-13 23:20:53 +0300

    Person OOP

--- CodeWars/person_Class.py
+++ CodeWars/person_Class.py
@@ -13,9 +13,32 @@ class Person:
         return self.__str__()
 
 
+class People:
+    def __init__(self, people=None):
+        if people is None:
+            people = []
+        self.people = people
+
+    def add_Person(self, person):
+        if isinstance(person, Person):
+            self.people.append(person)
+        else:
+            return 'Invalid person object'
+
+    def __str__(self):
+        return f'Greetings! There are {len(self.people)} people in the list.\n' + ', '.join(str(person) for person in self.people)
+
+    def __repr__(self):
+        return self.__str__()
+
+
 girl = Person('Maria', 15)
 boy = Person()
-boy.name = 'Sahsa'
+boy.name = 'Sasha'
 boy.age = 19
 print(girl)
 print(boy)
+people = People()
+people.add_Person(girl)
+people.add_Person(boy)
+print(people)
